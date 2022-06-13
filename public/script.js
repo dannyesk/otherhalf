55589ba932c08bf23cc7aecaa47e2b1d1ca0ea9c
dannyesk <danieles.kay.i@gmail.com>
2022-06-12 21:21:44 -0700

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,24 @@
+/*
+$(document).ready(function() {
+	
+	
+	
+ // run test on initial page load
+    checkSize();
+      $(window).resize(checkSize);
+console.log("jQuery is Ready!");
+
+
+				$('.right').click(function(){
+						$('.right').css("cursor", "pointer");
+						$("#contenright").css("display", "block");
+						$("#imgright").css("display", "none");
+						$("#contenleft").css("display", "none");
+						$("#imgleft").css("display", "block");
+						});
+
+				
+				
+				
+}); 
+*/
